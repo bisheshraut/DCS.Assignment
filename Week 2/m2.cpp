19c90d20dfa6984299749552977e9ffebb0e9537
BisheshR <bishesh.raut@westcliff,edu>
2025-11-16 10:06:11 -0600

    Week 3 assignments

--- Week 2/m2.cpp
+++ Week 2/m2.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main() {
     double angle;
+    
     cout << "Enter angle in radians: ";
     cin >> angle;
-
-    cout << "sin(angle) = " << sin(angle) << endl;
-    cout << "cos(angle) = " << cos(angle) << endl;
-    cout << "tan(angle) = " << tan(angle) << endl;
-
+    
+    double sineValue = sin(angle);
+    double cosineValue = cos(angle);
+    double tangentValue = tan(angle);
+    
+    double degrees = angle * 180 / M_PI;
+    
+    cout << fixed << setprecision(3);
+    cout << "sin(" << degrees << "°) = " << sineValue << endl;
+    cout << "cos(" << degrees << "°) = " << cosineValue << endl;
+    cout << "tan(" << degrees << "°) = " << tangentValue << endl;
+    
     return 0;
 }
